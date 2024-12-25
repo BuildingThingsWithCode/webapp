ad0f450c1c5b30be8940fd5ac235b51e917e7a95
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-25 19:40:14 +0100

    Make home page dynamic

--- src/main/java/com/spring/webapp/MainController.java
+++ src/main/java/com/spring/webapp/MainController.java
@@ -1,13 +1,16 @@
 package com.spring.webapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/home")
-	public String home() {
-		return "home";
+	public String home(Model page) {
+		page.addAttribute("username", "James");
+		page.addAttribute("color", "yellow");
+		return "home.html";
 	}
 }
